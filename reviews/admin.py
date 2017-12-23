247a9984b9b62a0fa0e6458a6e808c95951b6abe
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2017-12-23 12:58:14 +0530

    Recommendation using KMeans not working. Ashwin look.

---
+++ reviews/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Wine, Review, Cluster
+# Register your models here.
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ['wine', 'rating', 'user_name', 'comments', 'pub_date']
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comments']
+
+
+class ClusterAdmin(admin.ModelAdmin):
+    model = Cluster
+    list_display = ['name', 'get_members']
+
+
+admin.site.register(Wine)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Cluster, ClusterAdmin)
---
+++ reviews/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+import numpy as np
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Wine(models.Model):
+    name = models.CharField(max_length=200)
+
+    def average_rating(self):
+        all_ratings = map(lambda x: x.rating, self.review_set.all())
+        return np.mean(all_ratings)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+
+    wine = models.ForeignKey(Wine)
+    pub_date = models.DateTimeField("date_published")
+    user_name = models.CharField(max_length=100)
+    comments = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES)
+
+
+class Cluster(models.Model):
+    name = models.CharField(max_length=100)
+    users = models.ManyToManyField(User)
+
+    def get_members(self):
+        return "\n".join([u.username for u in self.users.all()])
